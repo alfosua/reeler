7a5f8289aea348a11a18991aee98f5629294cb00
Alfonso Suarez <asuarez@asptg.com>
2024-09-30 01:11:01 -0400

    refactor: move viewmodel logic away from downloader screen composable base

--- mobile/app/src/main/java/com/catalinalabs/reeler/ui/screens/DownloaderScreen.kt
+++ mobile/app/src/main/java/com/catalinalabs/reeler/ui/screens/DownloaderScreen.kt
@@ -52,7 +52,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import com.catalinalabs.reeler.R
@@ -73,12 +72,35 @@ fun DownloaderScreen(
     saveVideoToMediaStore: (ContentResolver, ByteArray, VideoInfoOutput) -> String = ::saveVideoToMediaStore,
     showInterstitialAd: (Context) -> Unit = ::showInterstitialAd,
 ) {
+    DownloaderScreen(
+        status = viewModel.status,
+        videoInfo = viewModel.videoInfo,
+        sourceUrl = viewModel.sourceUrl,
+        processVideoInfo = viewModel::processVideoInfo,
+        processDownload = viewModel::processDownload,
+        setVideoUrl = viewModel::setVideoUrl,
+        saveVideoToMediaStore = saveVideoToMediaStore,
+        showInterstitialAd = showInterstitialAd,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun DownloaderScreen(
+    sourceUrl: String,
+    status: DownloadProcessStatus,
+    videoInfo: VideoInfoOutput?,
+    processVideoInfo: () -> Unit,
+    processDownload: ((ByteArray, VideoInfoOutput) -> String) -> Unit,
+    setVideoUrl: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    saveVideoToMediaStore: (ContentResolver, ByteArray, VideoInfoOutput) -> String = ::saveVideoToMediaStore,
+    showInterstitialAd: (Context) -> Unit = ::showInterstitialAd,
+) {
     val context = LocalContext.current
     val resolver = context.contentResolver
-    val status = viewModel.status
-    val videoInfo = viewModel.videoInfo
-    val processVideoInfo = {
-        viewModel.processVideoInfo()
+    val processVideoInfoAndShowInterstitial = {
+        processVideoInfo()
         showInterstitialAd(context)
     }
 
@@ -86,15 +108,15 @@ fun DownloaderScreen(
         if (context is Activity && context.intent?.action == Intent.ACTION_SEND) {
             val urlFromIntent = context.intent.extras?.getString(Intent.EXTRA_TEXT)
             if (urlFromIntent != null) {
-                viewModel.setVideoUrl(urlFromIntent)
-                processVideoInfo()
+                setVideoUrl(urlFromIntent)
+                processVideoInfoAndShowInterstitial()
             }
         }
     }
 
     if (status is DownloadProcessStatus.ProcessingSuccess) {
         LaunchedEffect(Unit) {
-            viewModel.processDownload { data, videoInfo ->
+            processDownload { data, videoInfo ->
                 saveVideoToMediaStore(resolver, data, videoInfo)
             }
         }
@@ -102,8 +124,8 @@ fun DownloaderScreen(
 
     Column(modifier) {
         DownloadField(
-            videoUrl = viewModel.sourceUrl,
-            onVideoUrlChange = viewModel::setVideoUrl,
+            videoUrl = sourceUrl,
+            onVideoUrlChange = setVideoUrl,
             onDownloadButtonClick = processVideoInfo,
             modifier = Modifier.fillMaxWidth(),
         )
@@ -119,7 +141,7 @@ fun DownloaderScreen(
             )
         }
         DownloadProcessStatusTracker(
-            viewModel.status,
+            status,
             modifier = Modifier.fillMaxWidth(),
         )
     }
@@ -130,8 +152,25 @@ fun DownloaderScreen(
 fun DownloaderScreenPreview() {
     ReelerTheme {
         DownloaderScreen(
-            viewModel = viewModel(),
+            setVideoUrl = { },
+            processDownload = { },
+            saveVideoToMediaStore = { _,_,_ -> "" },
+            showInterstitialAd = { },
+            processVideoInfo = { },
+            sourceUrl = "https://instagram.com/",
+            status = DownloadProcessStatus.DownloadSuccess,
             modifier = Modifier.fillMaxHeight(),
+            videoInfo = VideoInfoOutput(
+                filename = "ig-downloader-1727597487.mp4",
+                contentUrl = "https://instagram.alfosuag.workers.dev/video-download/aHR0cHM6Ly9zY29udGVudC15eXoxLTEuY2RuaW5zdGFncmFtLmNvbS9vMS92L3QxNi9mMS9tODIvRUY0MjE3NTkyQ0IyMDBEM0U2QTAzMDIyMzM0M0E5QUNfdmlkZW9fZGFzaGluaXQubXA0P3N0cD1kc3QtbXA0JmVmZz1leUp4WlY5bmNtOTFjSE1pT2lKYlhDSnBaMTkzWldKZlpHVnNhWFpsY25sZmRuUnpYMjkwWmx3aVhTSXNJblpsYm1OdlpHVmZkR0ZuSWpvaWRuUnpYM1p2WkY5MWNteG5aVzR1WTJ4cGNITXVZekl1TnpJd0xtSmhjMlZzYVc1bEluMCZfbmNfY2F0PTExMCZ2cz04MTA3NTA1OTA5MTQ0ODRfMjYzMDQyMzQ1OCZfbmNfdnM9SEJrc0ZRSVlUMmxuWDNod2RsOXlaV1ZzYzE5d1pYSnRZVzVsYm5SZmNISnZaQzlGUmpReU1UYzFPVEpEUWpJd01FUXpSVFpCTURNd01qSXpNelF6UVRsQlExOTJhV1JsYjE5a1lYTm9hVzVwZEM1dGNEUVZBQUxJQVFBVkFoZzZjR0Z6YzNSb2NtOTFaMmhmWlhabGNuTjBiM0psTDBkSExYSldhSEl0YTNCeFVYZ3lhMDVCUjFOd2ExVlJSR2hYU1ZsaWNWOUZRVUZCUmhVQ0FzZ0JBQ2dBR0FBYkFCVUFBQ2FXblBHUHhlT0ZRUlVDS0FKRE15d1hRRE16TXpNek16TVlFbVJoYzJoZlltRnpaV3hwYm1WZk1WOTJNUkVBZGY0SEFBJTNEJTNEJl9uY19yaWQ9YjI2MGVjMGE4MyZjY2I9OS00Jm9oPTAwX0FZRGo1eVV5MGZHdThrMm4zbmhVQlVLMTBoS3MxcGFrZkJkM2thSG1nRHJUdkEmb2U9NjZGQUVEMjQmX25jX3NpZD0xMGQxM2I=.mp4",
+                width = 750,
+                height = 1333,
+                username = "cris_villegas07",
+                caption = "Part 1 | Jujutsu Kaisen 222-235 manga chapters Animation 🔥",
+                duration = 19.2,
+                userAvatarUrl = "https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-19/457621679_877143554292925_4043951588544897022_n.jpg?stp=dst-jpg_e0_s150x150&_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=101&_nc_ohc=BsuRG3nmJBYQ7kNvgHKLVtG&_nc_gid=b260e280f8ee4b079f61b4c67b4b2c92&edm=APs17CUBAAAA&ccb=7-5&oh=00_AYBkT_2-MZ4SwYe5vD9FCmwdMLvxhJT03wn43vDi9cDFmA&oe=66FEEF3C&_nc_sid=10d13b",
+                thumbnailUrl = "https://scontent-yyz1-1.cdninstagram.com/v/t51.29350-15/441015281_460146716487560_3290678782543390784_n.jpg?stp=c0.280.720.720a_dst-jpg_e15_s640x640&_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=108&_nc_ohc=6VUypcyjz54Q7kNvgHCfifs&_nc_gid=b260e280f8ee4b079f61b4c67b4b2c92&edm=APs17CUBAAAA&ccb=7-5&oh=00_AYBSTuioxdGQnAKVeFPWaHcr9efa18de73IH0RDqQqrmkg&oe=66FEDE70&_nc_sid=10d13b"
+            ),
         )
     }
 }
