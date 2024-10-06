5891f166c7332e602b4e6ea72d0b5574e9e2cf85
Alfonso Suarez <68227851+alfosua@users.noreply.github.com>
2024-10-06 01:12:35 -0400

    feat: setup launcher icon, add it on top bar

--- mobile/app/src/main/java/com/catalinalabs/reeler/ui/TopAppBar.kt
+++ mobile/app/src/main/java/com/catalinalabs/reeler/ui/TopAppBar.kt
@@ -1,47 +1,73 @@
 package com.catalinalabs.reeler.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.SlowMotionVideo
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.catalinalabs.reeler.R
+import com.catalinalabs.reeler.ui.theme.ReelerTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopAppBar(modifier: Modifier = Modifier) {
+    val isLight = MaterialTheme.colorScheme.background.luminance() > 0.5
     CenterAlignedTopAppBar(
         modifier = modifier,
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            containerColor =
+                if (isLight) MaterialTheme.colorScheme.onPrimaryContainer
+                else MaterialTheme.colorScheme.primaryContainer,
         ),
         title = {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
             ) {
-                Icon(
-                    imageVector = Icons.Outlined.SlowMotionVideo,
+                Image(
+                    painter =
+                        if (isLight) painterResource(R.drawable.reeler_color_icon)
+                        else painterResource(R.drawable.reeler_light_icon),
                     contentDescription = null,
-                    tint = MaterialTheme.colorScheme.onPrimaryContainer,
+                    modifier = Modifier.width(24.dp),
                 )
                 Spacer(Modifier.width(8.dp))
                 Text(
                     text = "Reeler",
                     style = MaterialTheme.typography.titleLarge,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    color =
+                        if (isLight) MaterialTheme.colorScheme.primaryContainer
+                        else MaterialTheme.colorScheme.onPrimaryContainer,
                     fontWeight = FontWeight.Bold,
                 )
             }
         },
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopAppBarPreview() {
+    ReelerTheme {
+        TopAppBar()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopAppBarPreviewWithDarkTheme() {
+    ReelerTheme(darkTheme = true) {
+        TopAppBar()
+    }
 }
