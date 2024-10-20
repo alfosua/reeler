4935ec4b8cade0392a337cb49c2af7bdef574bc6
Alfonso Suarez <68227851+alfosua@users.noreply.github.com>
2024-10-20 14:36:33 -0400

    fix: sanitize better the caption for the filename

---
+++ mobile/app/src/main/java/com/catalinalabs/reeler/workers/Filenames.kt
@@ -0,0 +1,37 @@
+package com.catalinalabs.reeler.workers
+
+import java.text.Normalizer
+
+fun getTimestampString(): String {
+    val timestamp = System.currentTimeMillis()
+    return timestamp.toString()
+}
+
+fun getFilenameForMedia(
+    caption: String?,
+    sourceId: String?,
+    extension: String = "mp4",
+): String {
+    val timestamp = getTimestampString()
+    val sanitized = caption?.let { sanitizeCaptionForFilename(caption) }
+    return when {
+        sanitized != null && sourceId != null -> "${sanitized}_[${sourceId}].$extension"
+        sourceId != null -> "[$sourceId].$extension"
+        sanitized != null -> "${sanitized}_[$timestamp].$extension"
+        else -> "$timestamp.$extension"
+    }
+}
+
+fun sanitizeCaptionForFilename(caption: String): String {
+    var filename = caption.trim()
+        .replace(Regex("[^A-Za-z0-9_\\-]"), "_")
+
+    filename = Normalizer.normalize(filename, Normalizer.Form.NFD)
+    filename = filename.replace(Regex("[\\p{InCombiningDiacriticalMarks}]"), "")
+
+    if (filename.length > 100) {
+        filename = filename.substring(0, 100)
+    }
+
+    return filename
+}
